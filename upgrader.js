9d3127c9cdccc9fee9569f0c9c1ca925fbd8e023
MonkeyToiletLadder <32585563+MonkeyToiletLadder@users.noreply.github.com>
2020-10-16 21:51:03 -0700

    Update upgrader.js
    Added spawn and tick functions.  Uses helpers file.

--- upgrader.js
+++ upgrader.js
@@ -1,3 +1,42 @@
+let helpers = require('helpers.js');
+let consts = require('constants.js');
+
 module.exports = {
-    
+    spawn: function(spawn, body, target, room) {
+        spawn = Game.spawns[spawn];
+        if(_.isUndefined(spawn)) {
+            return consts.ERROR_INVALID_ARGS;
+        }
+        target = Game.flags[target];
+        if(_.isUndefined(target)) {
+            return consts.ERROR_INVALID_ARGS;   
+        }
+        
+        if(_.isUndefined(target.memory.harvester)) {
+            return consts.ERROR_NO_HARVESTERS;   
+        }
+        let creep = Date.now();
+        
+        let is_spawned = spawn.spawnCreep(body, creep, { memory: { role: 'upgrader', tags: ['hauler'], is_gathering: true, target: target.name } });
+        
+        return is_spawned;
+    },
+    tick: function(upgrader) {
+        if(upgrader.memory.is_gathering && upgrader.store.getFreeCapacity() == 0) {
+            upgrader.memory.is_gathering = false;
+        } else if(!upgrader.memory.is_gathering && upgrader.store.getFreeCapacity() == upgrader.store.getCapacity()) {
+            upgrader.memory.is_gathering = true;
+        }
+        if(upgrader.memory.is_gathering) {
+            helpers.haul(upgrader, RESOURCE_ENERGY);
+        } else {
+            let rc = upgrader.room.controller;
+            
+            let is_transfered = upgrader.transfer(rc, RESOURCE_ENERGY);
+            
+            if(is_transfered == ERR_NOT_IN_RANGE) {
+                upgrader.moveTo(rc);
+            }
+        }        
+    }
 }
