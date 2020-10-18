97bfe5be6ca754d57f53cec2864df8529caf9677
MonkeyToiletLadder <32585563+MonkeyToiletLadder@users.noreply.github.com>
2020-10-17 20:34:54 -0700

    Update storer.js

--- storer.js
+++ storer.js
@@ -26,13 +26,13 @@ module.exports = {
         if(storer.memory.is_gathering) {
             let flag = Game.flags[storer.memory.target];
             let target = Game.getObjectById(flag.memory.target_id);
-            if(!_.isUndefined(target.memory.harvester)) {
+            if(!_.isUndefined(flag.memory.harvester)) {
                 helpers.haul(storer, RESOURCE_ENERGY);   
             } else {
                 let is_harvested = storer.harvest(target);
-            }
-            if(is_harvested == ERR_NOT_IN_RANGE) {
-                storer.moveTo(target);   
+                if(is_harvested == ERR_NOT_IN_RANGE) {
+                    storer.moveTo(target);   
+                }
             }
         } else {
             let stores = storer.pos.find(FIND_MY_STRUCTURES, { filter: (s) => { return !s.isUndefined(s.store) && s.store.getUsedCapacity() < s.store.getCapacity(); } })
